8c6da176c1f637b66f700bb697e2c6c143bcbc7c
Andrew LeCody <348675+aceat64@users.noreply.github.com>
2025-07-27 11:34:48 -0600

    cli: improved db migrate

--- backend/cli/db.py
+++ backend/cli/db.py
@@ -1,15 +1,23 @@
+from pathlib import Path
+from typing import Annotated
+
 import typer
+
+from app.core.logging import setup_logging
+
+from .config.utils import load_config
 
 cli_app = typer.Typer(help="Database migrations and tools.")
 
 
 @cli_app.command(help="Update database to latest schema.")
-def migrate() -> None:
+def migrate(revision: Annotated[str, typer.Argument()] = "head") -> None:
     from alembic import command
     from alembic.config import Config
 
-    alembic_cfg = Config("alembic.ini")
-    command.upgrade(alembic_cfg, "head")
+    setup_logging(load_config())
+    alembic_cfg = Config(f"{Path(__file__).parent.parent.resolve()}/alembic.ini")
+    command.upgrade(alembic_cfg, revision)
 
 
 # TODO: Remove this workaround once Typer merges this PR: https://github.com/fastapi/typer/pull/1240
